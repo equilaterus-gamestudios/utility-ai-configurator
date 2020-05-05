68fb9476b598256de94cea0d04c869fab221ff28
dacanizares <dacanizares@outlook.com>
2020-05-05 18:14:30 -0500

    Added core models
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ client/src/common/models.ts
@@ -0,0 +1,48 @@
+// ConditionEvaluator
+// Models
+
+export interface ConditionEvaluatorModel {
+  tag: string,
+  functionName: string,
+  parameters: Array<ParameterModel>
+  curve: Curve,
+  description: string,
+}
+
+export interface Curve {
+  curveType: string,
+  exponent: number,
+  slope: number,
+  xShift: number,
+  yShift: number,
+}
+
+export interface ParameterModel {
+  key: string,
+  value: string,
+  description: string,
+}
+
+
+// Decision
+// Models
+
+export interface DecisionModel {
+  tag: string,
+  functionName: string,
+  weight: number,
+  hasTargetDependency: boolean
+  conditionEvaluators: Array<ConditionEvaluatorModel>
+  description: string,
+}
+
+
+// Decision set
+// Models
+
+export interface DecisionSetModel {
+  tag: string,
+  decisions: Array<DecisionModel>,
+  defaultDecision: string,
+  description: string
+}
