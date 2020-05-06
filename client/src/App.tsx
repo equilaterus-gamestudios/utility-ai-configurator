b8766b7fd78436bfff03f70f4f75e418a45fb6d7
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-05 21:23:22 -0500

    Curve component WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootlaterus/dist/css/bootlaterus-cfonts.min.css';
 import './overrides.css';
 import Parameter from './components/editors/Parameter';
+import Curve from './components/editors/Curve';
 
 function App() {
   return (
     <div className="container">
       <Parameter />
+      <Curve curveType="POLINOMIAL" exponent={1} slope={-1} xShift={0.4} yShift={-0.5} />
     </div>
   );
 }
