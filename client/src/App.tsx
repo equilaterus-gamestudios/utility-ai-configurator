7877de312f5a3b21b77c87aa4f46d7127dcb5ea2
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-07 19:55:51 -0500

    Added default curve

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,21 @@ import Parameter from './components/editors/Parameter';
 
 import CurveEditor from './components/editors/Curve';
 
+import {Curve} from './common/models';
+
+const defaultCurve:Curve = {
+  curveType: 'POLINOMIAL',
+  exponent: 1,
+  slope: 1,
+  xShift: 1,
+  yShift: 1
+};
+
 function App() {
   return (
     <div className="container">
       <Parameter />
-      <CurveEditor />
+      <CurveEditor defaultCurve={defaultCurve} />
     </div>
   );
 }
