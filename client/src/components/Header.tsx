2a8b02f819297461baded9e515e04adc52ace17a
dacanizares <dacanizares@outlook.com>
2020-05-09 13:21:23 -0500

    Updated header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <nav className="navbar navbar-side navbar-side-left navbar-side-lg navbar-dark bg-dark-alpha-2 collapse" id="sideNavbar">
-  <div className="navbar-brand-container bg-dark-alpha-1">
-    <Link className="navbar-brand" to="/">
-      <span>Utility AI</span>
-    </Link>
-  </div>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#sideNavbar" aria-controls="sideNavbar" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <ul className="navbar-nav">
-    <li><Link className="nav-link" to="/ConditionEvaluators">Condition Evaluators</Link></li>
-    <li><Link className="nav-link" to="/Decisions">Decisions</Link></li>
-    <li><Link className="nav-link" to="/DecisionSets">Decision Sets</Link></li>
-  </ul>
-</nav>
-)
+const Header = () => {
+  const [collapsed, setCollapsed] = useState('collapse');
+  const onCollapse = () => {
+    setCollapsed(collapsed === '' ? 'collapse' : '')
+  }
+  const onHide = () => {
+    setCollapsed('collapse');
+  }
+  return (
+    <nav className={`navbar navbar-side navbar-side-left navbar-side-sm navbar-dark bg-dark-alpha-2 h-font ${collapsed}`}  id="sideNavbar">
+      <div className="navbar-brand-container bg-dark-alpha-1">
+        <Link onClick={onHide} className="navbar-brand" to="/">
+          <span>Utility AI</span>
+        </Link>
+      </div>
+      <button className="navbar-toggler" type="button" onClick={onCollapse} aria-controls="sideNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <ul className="navbar-nav">
+        <li><Link onClick={onHide} className="nav-link" to="/ConditionEvaluators">Condition Evaluators</Link></li>
+        <li><Link onClick={onHide} className="nav-link" to="/Decisions">Decisions</Link></li>
+        <li><Link onClick={onHide} className="nav-link" to="/DecisionSets">Decision Sets</Link></li>
+      </ul>
+    </nav>
+  )
+}
 
 export default Header;
