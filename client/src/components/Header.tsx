dcf952aa9009e375689794b838ea7afb2aa2aeaa
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-09 10:11:04 -0500

    Added links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <nav className="navbar navbar-side navbar-side-left navbar-side-lg navbar-dark bg-dark-alpha-2 collapse" id="sideNavbar">
   <div className="navbar-brand-container bg-dark-alpha-1">
-    <a className="navbar-brand" href="#">
-      <span>YOUR_TITLE</span>
-    </a>
+    <Link className="navbar-brand" to="/">
+      <span>Utility AI</span>
+    </Link>
   </div>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#sideNavbar" aria-controls="sideNavbar" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <ul className="navbar-nav">
-    <li><Link className="nav-link" to="/c">Nowhere</Link></li>
-    <li><a className="nav-link" href="#">Another link</a></li>
-    <li><a className="nav-link" href="#">Your link</a></li>
+    <li><Link className="nav-link" to="/ConditionEvaluators">Condition Evaluators</Link></li>
+    <li><Link className="nav-link" to="/Decisions">Decisions</Link></li>
+    <li><Link className="nav-link" to="/DecisionSets">Decision Sets</Link></li>
   </ul>
 </nav>
 )
