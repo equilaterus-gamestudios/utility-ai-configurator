ef10dfd77ff7d044c7b7c8a072323b6cf0dbb5e2
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-09 23:47:49 -0500

    Added generic table

---
+++ src/components/Table.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import RowContainer from '../containers/RowContainer';
+
+interface PropTypes {
+  tags: Array<string>,
+  properties: Array<string>,
+  actions: (any) => any,
+  selector: (any) => any
+}
+
+const renderBody = (tags : Array<string>, properties : Array<string>, actions : (any) => any, selector : (any) => any) => (
+  tags.map(tag => (
+    <tr>
+      <RowContainer key={tag} properties={properties} actions={actions} selector={selector(tag)} />  
+    </tr>
+  ))
+)
+
+const renderHeader = (properties : Array<string>, hasActions : boolean) => (
+  <tr>
+    {
+      properties.map(property => (
+        <th>{property}</th>      
+      ))
+    }  
+    {
+      hasActions && <th>Actions</th>  
+    }
+  </tr>
+)
+
+const Table = ({ tags, properties, actions, selector } : PropTypes) => {
+  return (
+    <table className="table">
+      <thead className="thead-dark">
+      { renderHeader(properties, !!actions) }
+      </thead>
+      <tbody>
+        { renderBody(tags, properties, actions, selector) }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
