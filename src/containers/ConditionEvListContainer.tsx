baa9c43a6339173918b1dd993964ab36be54ef20
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-09 23:52:04 -0500

    Removed unused code

--- src/containers/ConditionEvListContainer.tsx
+++ src/containers/ConditionEvListContainer.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { ConditionEvaluatorModel } from '../common/models'
 import { selectConditionEvaluatorTags, selectConditionEvaluatorByTag } from '../selectors/ConditionEvSelector';
-import ConditionEvaluatorRow from '../components/ConditionEvaluatorRow';
 import Table from '../components/Table';
 
 interface PropTypes {
