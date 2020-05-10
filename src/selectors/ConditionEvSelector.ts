5214e9f6b258d9eadc4a2e1e4ed2712be4507383
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-09 23:49:49 -0500

    Adjusted selectors to support normalization

--- src/selectors/ConditionEvSelector.ts
+++ src/selectors/ConditionEvSelector.ts
@@ -4,5 +4,6 @@ interface RootState {
   conditionEvaluators: ConditionEvaluatorState
 }
 
-export const selectConditionEvaluators = (state: RootState) => state.conditionEvaluators.data;
+export const selectConditionEvaluatorTags = (state: RootState) => state.conditionEvaluators.tags;
+export const selectConditionEvaluatorByTag = (tag) => (state: RootState) => state.conditionEvaluators.byTag[tag];
 
