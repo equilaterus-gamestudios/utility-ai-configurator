e6a60ac7433b0b2389270838d52a935037dd64d1
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-11 23:34:08 -0500

    ConditionEvaluator actions WIP

--- src/actions/conditionEvaluatorActions.ts
+++ src/actions/conditionEvaluatorActions.ts
@@ -1,4 +1,5 @@
-import { LOAD_CONDITION_EVALUATORS_SUCCESS, LOAD_CONDITION_EVALUATORS_REQUEST, ConditionEvaluatorActionTypes,  } from './types';
+import history from '../history';
+import { LOAD_CONDITION_EVALUATORS_SUCCESS, LOAD_CONDITION_EVALUATORS_REQUEST, SAVE_CONDITION_EVALUATOR_REQUEST, ConditionEvaluatorActionTypes } from './types';
 import  *  as conditionEvaluatorAPI  from '../api/conditionEvaluatorAPI';
 
 import { ConditionEvaluatorModel } from '../common/models';
@@ -9,7 +10,7 @@ const loadConditionEvaluatorsRequest = () : ConditionEvaluatorActionTypes => {
   }
 }
 
-const loadConditionEvaluatorsSuccess = (conditionEvaluators : [ConditionEvaluatorModel]) : ConditionEvaluatorActionTypes => {
+const loadConditionEvaluatorsSuccess = (conditionEvaluators : Array<ConditionEvaluatorModel>) : ConditionEvaluatorActionTypes => {
   return {
     type: LOAD_CONDITION_EVALUATORS_SUCCESS,
     payload: conditionEvaluators
@@ -20,4 +21,18 @@ export const loadConditionEvaluators = () => async (dispatch) => {
   dispatch(loadConditionEvaluatorsRequest());
   const conditionEvaluators = await conditionEvaluatorAPI.loadConditionEvaluators();
   dispatch(loadConditionEvaluatorsSuccess(conditionEvaluators));
+}
+
+
+const saveConditionEvaluator =  (conditionEvaluator : ConditionEvaluatorModel) : ConditionEvaluatorActionTypes => {
+  return {
+    type: SAVE_CONDITION_EVALUATOR_REQUEST,
+    payload: conditionEvaluator
+  }
+}
+
+
+export const saveConditionEvaluatorAndRedirect = (conditionEvaluator) => async (dispatch) => {
+  dispatch(saveConditionEvaluator(conditionEvaluator));
+  history.push('/ConditionEvaluators');
 }
