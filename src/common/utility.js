aa139db23cc93679eeeb382b34aafe3e542d08e3
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-12 18:46:56 -0500

    Added utility to stringify in Pascal Case
    Co-Authored-By: Daniel Cañizares Corrales <dacanizares@outlook.com>

--- src/common/utility.js
+++ src/common/utility.js
@@ -12,3 +12,27 @@ export const createByTagStructure = (list) => (
 export const getValuesFromByTag = (byTag) => (
   values(byTag)
 )
+
+export const StringifyToPascalCase = (obj) => (
+  JSON.stringify(obj, function (key, value) {
+    if (value && typeof value === 'object' && !Array.isArray(value)) {
+      var replacement = {};
+      for (var k in value) {
+        if (Object.hasOwnProperty.call(value, k)) {
+          replacement[k && k.charAt(0).toUpperCase() + k.substring(1)] = value[k];
+        }
+      }
+      return replacement;
+    }
+    
+    if (typeof value === 'boolean') {
+      return  value ? '1' : '0';
+    }
+
+    if (typeof value === 'number') {
+      return value.toString();
+    }
+
+    return value;
+  })
+)
