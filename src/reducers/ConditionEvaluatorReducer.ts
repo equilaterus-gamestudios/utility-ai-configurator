fc6c8953a89a35318d06a2e4515de3e248e93de2
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-11 23:31:36 -0500

    ConditionEvaluator reducer WIP

--- src/reducers/ConditionEvaluatorReducer.ts
+++ src/reducers/ConditionEvaluatorReducer.ts
@@ -1,13 +1,16 @@
 import { getTags, createByTagStructure } from '../common/utility';
 import { Dictionary, ConditionEvaluatorModel } from '../common/models';
-import { ConditionEvaluatorActionTypes, LOAD_CONDITION_EVALUATORS_SUCCESS } from '../actions/types';
+import { ConditionEvaluatorActionTypes, LOAD_CONDITION_EVALUATORS_SUCCESS, SAVE_CONDITION_EVALUATOR_REQUEST } from '../actions/types';
 
 export interface ConditionEvaluatorState {  
   byTag: Dictionary<ConditionEvaluatorModel>,
   tags: Array<string>
 }
 
-const defaultState = {} as ConditionEvaluatorState
+const defaultState = {
+  byTag: {},
+  tags: []
+} as ConditionEvaluatorState
 
 export const conditionEvaluatorReducer = (state = defaultState, action : ConditionEvaluatorActionTypes) : ConditionEvaluatorState  => {  
   switch(action.type) {
@@ -16,6 +19,23 @@ export const conditionEvaluatorReducer = (state = defaultState, action : Conditi
         ...state, 
         byTag: createByTagStructure(action.payload),
         tags: getTags(action.payload)
+      }
+    case SAVE_CONDITION_EVALUATOR_REQUEST:
+      {
+        const isNewConditionEvaluator = !state.tags.find(tag => tag === action.payload.tag);
+        const tags = [...state.tags];
+        if (isNewConditionEvaluator) {
+          tags.push(action.payload.tag);
+        }
+        
+        return {
+          ...state, 
+          byTag: {
+            ...state.byTag,
+            [action.payload.tag]: action.payload
+          },
+          tags: tags
+        } 
       }
     default: 
       return state;
