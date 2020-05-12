44500b5860286b85c2f739331c58465e86615878
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-11 23:31:17 -0500

    DecisionSet reducer WIP

--- src/reducers/DecisionSetReducer.ts
+++ src/reducers/DecisionSetReducer.ts
@@ -1,8 +1,8 @@
 import { getTags, createByTagStructure } from '../common/utility';
 import { Dictionary, DecisionSetModel } from '../common/models';
-import { DecisionSetActionTypes, LOAD_DECISION_SETS_SUCCESS } from '../actions/types';
+import { DecisionSetActionTypes, LOAD_DECISION_SETS_SUCCESS, SAVE_DECISION_SETS_REQUEST } from '../actions/types';
 
-interface DecisionSetState {  
+export interface DecisionSetState {  
   byTag: Dictionary<DecisionSetModel>,
   tags: Array<string>
 }
@@ -16,6 +16,23 @@ export const decisionSetReducer = (state = defaultState, action : DecisionSetAct
         ...state,
         byTag: createByTagStructure(action.payload),
         tags: getTags(action.payload)
+      }
+    case SAVE_DECISION_SETS_REQUEST:
+      {
+        const isNewDecisionSet = !state.tags.find(tag => tag === action.payload.tag);
+        const tags = [...state.tags];
+        if (isNewDecisionSet) {
+          tags.push(action.payload.tag);
+        }
+        
+        return {
+          ...state, 
+          byTag: {
+            ...state.byTag,
+            [action.payload.tag]: action.payload
+          },
+          tags: tags
+        } 
       }
     default: 
       return state;
