75135b18c7c3b5b4e5567db9a3dd5f7fdad068f9
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-11 23:35:49 -0500

    Curve editor WIP
    Co-Authored-By: Daniel Cañizares Corrales <dacanizares@outlook.com>

--- src/components/editors/Curve.tsx
+++ src/components/editors/Curve.tsx
@@ -1,7 +1,7 @@
-import React, { useMemo} from 'react'
+import React, { useMemo } from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
-import { useForm } from '../../hooks/useForm';
 import { Curve } from '../../common/models';
+import { predefinedCurves } from '../../common/defaultCurves';
 
 const POLINOMIAL = "POLINOMIAL";
 const range = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
@@ -11,7 +11,7 @@ const polinomialFunction = (exponent, slope, xShift, yShift) => x =>  {
 }
 
 const gaussianFunction = (exponent, slope, xShift, yShift) => x =>  {
-  return exponent * Math.exp(-slope * Math.pow((x - xShift) / yShift, 2));  
+  return exponent * Math.exp(-1 * Math.pow((x - xShift) / yShift, 2)) - slope;  
 }
 
 const CurvePreview = ({curveType, exponent, slope, xShift, yShift}) => { 
@@ -34,7 +34,7 @@ const CurvePreview = ({curveType, exponent, slope, xShift, yShift}) => {
    
   return (
     <LineChart width={600} height={300} data={data}
-          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          margin={{top: 5, right: 30, left: 20, bottom: 5}} className="m-auto">
       <XAxis dataKey="x"/>
       <YAxis/>
       <CartesianGrid strokeDasharray="3 3"/>
@@ -47,20 +47,57 @@ const CurvePreview = ({curveType, exponent, slope, xShift, yShift}) => {
 }
 
 export interface CurveProps {
-  defaultCurve: Curve,
+  curve: Curve,
+  setCurve: (string, any) => void
 }
 
-const CurveEditor = ({defaultCurve} : CurveProps) => {
-  const [curve, setCurveProperty] = useForm(defaultCurve);
+const CurveEditor = ({ curve, setCurve } : CurveProps) => {
+  const handleChange = (property, value) => {    
+    setCurve('curve', {
+      ...curve,
+      [property]: value
+    })
+  }
+
+  const renderPredefinedCurves = () => (
+    <div className="form-group col-md-3">
+      <label>Predefined Curves:</label>
+      <select className="form-control" onChange={(e) => setCurve('curve', predefinedCurves[e.target.value])}>
+        {
+          Object.keys(predefinedCurves).map(key => (
+            <option value={key} key={key}>{key}</option>
+          ))
+        }        
+      </select>      
+    </div>
+  )
  
   return (
     <>
-    <CurvePreview curveType={curve.curveType} exponent={curve.exponent} slope={curve.slope} xShift={curve.xShift} yShift={curve.yShift} />
-        <label>Curve Type</label><select onChange={(e)=>setCurveProperty('curveType',e.currentTarget.options[e.currentTarget.selectedIndex].value)}><option value="POLINOMIAL">Polinomial</option><option value="GAUSSIAN">Gaussian</option></select>     
-        <label>Exponent</label> <input type="number" defaultValue="1" min="-2" step="1" onChange={(e)=>setCurveProperty('exponent',parseFloat(e.target.value))} />
-        <label>Slop</label>     <input type="number" defaultValue="1" step="0.01"       onChange={(e)=>setCurveProperty('slope',parseFloat(e.target.value))} />
-        <label>XShift</label>   <input type="number" defaultValue="1" step="0.01"       onChange={(e)=>setCurveProperty('xShift',parseFloat(e.target.value))} />
-        <label>YShift</label>   <input type="number" defaultValue="1" step="0.01"       onChange={(e)=>setCurveProperty('yShift',parseFloat(e.target.value))} />      
+      { renderPredefinedCurves() }
+      <CurvePreview curveType={curve.curveType} exponent={curve.exponent} slope={curve.slope} xShift={curve.xShift} yShift={curve.yShift} />
+
+      <div className="form-row">
+        <div className="form-group col-md-6">
+          <label>Exponent:</label>
+          <input type="number" className="form-control" value={curve.exponent} onChange={(e) => handleChange('exponent',parseFloat(e.target.value))} />
+        </div>
+        <div className="form-group col-md-6">
+          <label>Slop:</label>
+          <input type="number" className="form-control" value={curve.slope} step="0.01" onChange={(e) => handleChange('slope',parseFloat(e.target.value))} />
+        </div>
+      </div>
+
+      <div className="form-row">
+        <div className="form-group col-md-6">
+          <label>XShift:</label>
+          <input type="number" className="form-control" value={curve.xShift} step="0.01" onChange={(e) => handleChange('xShift',parseFloat(e.target.value))} />
+        </div>
+        <div className="form-group col-md-6">
+          <label>YShift:</label>
+          <input type="number" className="form-control" value={curve.yShift} step="0.01" onChange={(e) => handleChange('yShift',parseFloat(e.target.value))} />
+        </div>
+      </div>   
     </>
   );
 }
