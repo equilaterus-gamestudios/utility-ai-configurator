eeaa037cdfe7c48b692056e3f193e5ad17952f36
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-12 18:50:35 -0500

    Added export configuration actions
    Co-Authored-By: Daniel Cañizares Corrales <dacanizares@outlook.com>

---
+++ src/actions/exportConfigurationActions.ts
@@ -0,0 +1,26 @@
+import { getValuesFromByTag } from '../common/utility';
+import { EXPORT_CONFIGURATION_REQUEST, EXPORT_CONFIGURATION_SUCCESS, ExportConfigurationActionTypes } from './types';
+import  *  as configurationAPI  from '../api/configurationAPI';
+
+import { ConditionEvaluatorModel } from '../common/models';
+
+const exportCondigurationRequest = () : ExportConfigurationActionTypes => {
+  return {
+    type: EXPORT_CONFIGURATION_REQUEST
+  }
+}
+
+const exportCondigurationSuccess = () : ExportConfigurationActionTypes => {
+  return {
+    type: EXPORT_CONFIGURATION_SUCCESS
+  }
+}
+
+export const exportConfiguration = () => async (dispatch, getState) => {
+  dispatch(exportCondigurationRequest());
+  const conditionEvaluators = getValuesFromByTag(getState().conditionEvaluators.byTag);
+  const decisions = getValuesFromByTag(getState().decisions.byTag);
+  const decisionSets = getValuesFromByTag(getState().decisionSets.byTag);
+  await configurationAPI.exportConfiguration(conditionEvaluators, decisions, decisionSets);
+  dispatch(exportCondigurationSuccess());
+}
