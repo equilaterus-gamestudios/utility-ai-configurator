7a4c752433bb032a4dcfc30b056869d14a014972
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-11 23:35:31 -0500

    Added default curves

---
+++ src/common/defaultCurves.ts
@@ -0,0 +1,76 @@
+import { Curve } from './models';
+
+const lineal : Curve = {
+  curveType: 'POLINOMIAL',
+  exponent: 1,
+  slope: 1,
+  xShift: 1,
+  yShift: 1
+};
+
+const linealInverse : Curve = {
+  curveType: 'POLINOMIAL',
+  exponent: 1,
+  slope: -1,
+  xShift: 0,
+  yShift: -1
+};
+
+const quadratic : Curve = {
+  curveType: 'POLINOMIAL',
+  exponent: 2,
+  slope: 1,
+  xShift: 1,
+  yShift: 1
+};
+
+const quadraticInverse : Curve = {
+  curveType: 'POLINOMIAL',
+  exponent: 2,
+  slope: -1,
+  xShift: 0,
+  yShift: -1
+};
+
+const gaussian : Curve = {
+  curveType: 'GAUSSIAN',
+  exponent: 1,
+  slope: 0,
+  xShift: 0.5,
+  yShift: 0.3
+};
+
+const gaussianInverse : Curve = {
+  curveType: 'GAUSSIAN',
+  exponent: -1,
+  slope: -1,
+  xShift: 0.5,
+  yShift: 0.20
+};
+
+const sCurve : Curve = {
+  curveType: 'GAUSSIAN',
+  exponent: 1,
+  slope: 0,
+  xShift: 1,
+  yShift: 0.5
+};
+
+const sCurveInverse : Curve = {
+  curveType: 'GAUSSIAN',
+  exponent: 1,
+  slope: 0,
+  xShift: 0,
+  yShift: 0.5
+};
+
+export const predefinedCurves = {
+  lineal,
+  linealInverse,
+  quadratic,
+  quadraticInverse,
+  gaussian,
+  gaussianInverse,
+  sCurve,
+  sCurveInverse
+}
