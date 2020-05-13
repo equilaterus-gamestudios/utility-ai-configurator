5c3f2eb0ea55c706dfdbce928f81c768c7e1e7ea
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-13 18:34:46 -0500

    Added select all decisions
    Co-Authored-By: Daniel Cañizares Corrales <dacanizares@outlook.com>

--- src/selectors/DecisionSelector.ts
+++ src/selectors/DecisionSelector.ts
@@ -5,4 +5,5 @@ interface RootState {
 }
 
 export const selectDecisionTags = (state: RootState) => state.decisions.tags;
+export const selectAllDecisionsByTag = (state: RootState) => state.decisions.byTag;
 export const selectDecisionByTag = (tag) => (state: RootState) => state.decisions.byTag[tag];
