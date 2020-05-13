0a5e14776698f4c3fca65b0f79ff7a1a84c5d5e2
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-13 18:34:10 -0500

    Implemented remove decision and condition evaluator action in the reducer
    Co-Authored-By: Daniel Cañizares Corrales <dacanizares@outlook.com>

--- src/reducers/DecisionReducer.ts
+++ src/reducers/DecisionReducer.ts
@@ -1,6 +1,8 @@
 import { getTags, createByTagStructure } from '../common/utility';
 import { Dictionary, DecisionModel } from '../common/models';
-import { DecisionActionTypes, LOAD_DECISIONS_SUCCESS, SAVE_DECISIONS_REQUEST } from '../actions/types';
+import { DecisionActionTypes, LOAD_DECISIONS_SUCCESS, SAVE_DECISIONS_REQUEST, REMOVE_DECISION_REQUEST, REMOVE_CONDITION_EVALUATOR_REQUEST } from '../actions/types';
+import omit from 'lodash/omit';
+import mapValues from 'lodash/mapValues';
 
 export interface DecisionState {  
   byTag: Dictionary<DecisionModel>,
@@ -35,6 +37,28 @@ export const decisionReducer = (state = defaultState, action : DecisionActionTyp
             [action.payload.tag]: action.payload
           },
           tags: tags
+        } 
+      }
+    case REMOVE_DECISION_REQUEST:
+      {
+        const tags = state.tags.filter(tag => tag !== action.payload);
+        const byTag = omit(state.byTag, action.payload);
+        
+        return {
+          ...state, 
+          byTag,
+          tags
+        } 
+      }
+    case REMOVE_CONDITION_EVALUATOR_REQUEST:
+      { 
+        const byTag = mapValues(state.byTag, (decision) => ({
+          ...decision, conditionEvaluators: decision.conditionEvaluators.filter(ce => ce !== action.payload)
+        }))
+        
+        return {
+          ...state, 
+          byTag
         } 
       }
     default: 
