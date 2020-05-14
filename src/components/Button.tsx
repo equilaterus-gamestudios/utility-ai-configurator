cbf3b61e6e01cfe50b0925c1722edfd4da6adc09
dacanizares <dacanizares@outlook.com>
2020-05-13 21:34:35 -0500

    Added base button

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React, { FC, SyntheticEvent, memo } from 'react';
+
+export interface ButtonProps {
+  onClick: () => any
+  children?: React.ReactNode
+  [x: string]: any | undefined
+}
+
+const Button: FC<ButtonProps> = ({ onClick, children, ...otherProps }: ButtonProps) => {
+  const onClickHandler = (event: SyntheticEvent) => {
+    event.preventDefault();
+    onClick();
+  }
+
+  return (
+    <button 
+      onClick={onClickHandler}
+      {...otherProps}
+    >
+      { children }
+    </button>
+  )
+}
+
+export default memo(Button);
