cc612340334e47d479265b32e24ff7556cc5fe01
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2020-05-16 19:11:50 -0500

    Adjusted exporter code to support parameter dictionary in UE4

--- src/api/configurationAPI.ts
+++ src/api/configurationAPI.ts
@@ -3,6 +3,7 @@ import { DecisionModel, DecisionSetModel, ConditionEvaluatorModel } from '../com
 import { StringifyToPascalCase } from '../common/utility';
 
 import pick from 'lodash/pick';
+import Parameter from '../components/editors/Parameter';
 
 const FILE_NAME = 'AIConfiguration.json';
 
@@ -10,9 +11,18 @@ const conditionEvaluatorProperties = ['tag', 'functionName', 'curve', 'parameter
 const decisionProperties = ['tag', 'functionName', 'weight', 'hasTargetDependency', 'conditionEvaluators'];
 const decisionSetProperties = ['tag', 'defaultDecision', 'decisions'];
 
+const parametersToDictionary = (parameters) => {
+  const paramatersDic = {};
+  for (let i = 0; i < parameters.length; ++i) {
+    const parameter = parameters[i];
+    paramatersDic[parameter.key] = parameter.value;
+  }
+  return paramatersDic;
+}
+
 export const exportConfiguration = async (conditionEvaluators: Array<ConditionEvaluatorModel>,
   decisions : Array<DecisionModel>, decisionSets : Array<DecisionSetModel>) => {   
-  const procesedConditionEvaluators = conditionEvaluators.map(ce => pick(ce, conditionEvaluatorProperties));
+  const procesedConditionEvaluators = conditionEvaluators.map(ce => pick({...ce, parameters: parametersToDictionary(ce.parameters)}, conditionEvaluatorProperties));
   const procesedDecisions = decisions.map(d => pick(d, decisionProperties));
   const procesedDecisionSets = decisionSets.map(ds => pick(ds, decisionSetProperties));
   const configuration = {
